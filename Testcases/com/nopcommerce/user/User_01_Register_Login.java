a92cec227b560275f8200902e7ebce02d1e20162
Huong <tranthihuong821994@gmail.com>
2021-05-23 10:19:19 +0700

    Update TC_01

--- Testcases/com/nopcommerce/user/User_01_Register_Login.java
+++ Testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -1,12 +1,23 @@
 package com.nopcommerce.user;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class User_01_Register_Login {
+	WebDriver driver;
+	String projectLocation = System.getProperty("user.dir");
+
 	@BeforeClass
 	public void beforeClass() {
+		 System.setProperty("webdriver.gecko.driver", projectLocation +  ".\\browserDrivers\\geckodriver.exe");
+		 driver = new FirefoxDriver();
+		 //System.setProperty("webdriver.edge.driver", projectLocation +  ".\\BrowserDrivers\\msedgedriver.exe");
+		 //driver = new EdgeDriver();
+		 driver.get("https://demo.nopcommerce.com/");
 
 	}
 
@@ -16,11 +27,9 @@ public class User_01_Register_Login {
 	}
 
 	@AfterClass
-    public void afterClass() {
-	
+	public void afterClass() {
+		driver.quit();
+
+	}
+
 }
-
-
-
-}
-
